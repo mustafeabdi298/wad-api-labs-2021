c481f032d73f3a5040bc45c6a98e99f386500837
mustafeabdi298 <59968812+mustafeabdi298@users.noreply.github.com>
2021-12-10 22:23:16 +0000

    Add files via upload

---
+++ reactApp/src/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import { PublicPage, Movies, Profile, HomePage } from "./pages";
+import LoginPage from "./loginPage";
+import SignUpPage from "./signUpPage";
+import AuthProvider from "./authContext";
+import MovieProvider from "./moviesContext"
+import PrivateRoute from "./privateRoute";
+import AuthHeader from "./authHeader";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <AuthHeader />
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/public">Public</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+        <MovieProvider>
+        <Switch>
+          <Route path="/public" component={PublicPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage}/>
+          <PrivateRoute path="/movies" component={Movies} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <Redirect from="*" to="/" />
+        </Switch>
+        </MovieProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
